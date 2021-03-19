b81b8a195c73e1cb74247a08252e789411d51247
WiegnerO <30533050+WiegnerO@users.noreply.github.com>
2021-03-18 21:35:43 -0400

    Update globalVars.ts

--- src/app/services/globalVars.ts
+++ src/app/services/globalVars.ts
@@ -1,6 +1,6 @@
 
 'use strict';
 
-export const BASE_URL = 'https://myboard-server.herokuapp.com/api';
+export const BASE_URL = 'http://3.142.187.197/api';
 //export const BASE_URL = 'http://localhost:5000/api';
 export const LETTERS = /^[A-Za-z0-9 ]+$/;
